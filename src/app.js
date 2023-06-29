fd3a4edffcb500fa9e35ef91a1d1cb5fb0119776
Emiliano Carreño <emiliano0662@gmail.com>
2023-06-28 23:02:35 -0500

    inicio header, footer y slider

--- src/app.js
+++ src/app.js
@@ -7,3 +7,22 @@ Swiper.use([Autoplay, Navigation, Pagination, Thumbs]);
 /*All 'load' load the recurring blocks*/
 $("header").load("header.html");
 $("footer").load("footer.html");
+
+/*Script swiper*/
+const swiper = new Swiper('#slider-main', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
